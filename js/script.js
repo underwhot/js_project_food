1d07ecb01bb00f3e2d09ee5fc73377c318a9c7d2
Artem Petrovskiy <artpetrovskyi@gmail.com>
2023-07-28 20:09:53 +0300

    add more classes functional

--- js/script.js
+++ js/script.js
@@ -153,13 +153,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Menu cards class
   class MenuCard {
-    constructor(imgSrc, alt, title, desc, price, parentSelector) {
+    constructor(imgSrc, alt, title, desc, price, parentSelector, ...classes) {
       this.imgSrc = imgSrc;
       this.alt = alt;
       this.title = title;
       this.desc = desc;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
       this.transfer = 37;
       this.changeToUAH();
     }
@@ -170,18 +171,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     renderCard() {
       const elem = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.elem = "menu__item";
+        elem.classList.add(this.elem);
+      } else {
+        this.classes.forEach((className) => elem.classList.add(className));
+      }
       elem.innerHTML = `
-        <div class="menu__item">
-          <img src=${this.imgSrc} alt=${this.alt} >
-          <h3 class="menu__item-subtitle">${this.title}</h3>
-          <div class="menu__item-descr">${this.desc}</div>
-          <div class="menu__item-divider"></div>
-          <div class="menu__item-price">
-              <div class="menu__item-cost">Цена:</div>
-              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-          </div>
+        <img src=${this.imgSrc} alt=${this.alt} >
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.desc}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
-        `;
+      `;
 
       this.parent.append(elem);
     }
@@ -202,7 +207,10 @@ document.addEventListener("DOMContentLoaded", function () {
     'Меню "Премиум"',
     'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     15,
-    ".menu .container"
+    ".menu .container",
+    "menu__item",
+    'big',
+    'additional'
   ).renderCard();
 
   new MenuCard(
@@ -211,6 +219,7 @@ document.addEventListener("DOMContentLoaded", function () {
     'Меню "Постное"',
     'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     12,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).renderCard();
 });
